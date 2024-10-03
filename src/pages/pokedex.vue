f2b547fea3e6867baf9220a479627ca673e15fc3
Jared Ricioli <jared.ricioli@live.com>
2024-10-03 18:49:11 -0300

    voltei o listar para a pagina pokedex

--- src/pages/pokedex.vue
+++ src/pages/pokedex.vue
@@ -10,10 +10,37 @@
             </v-card-subtitle>
             <span class="d-flex flex-wrap span-poke">
                 <v-card-text class="pokemons">
-                    <ListarPokemons />
+                    <v-infinite-scroll :pokeList="pokeList" :onLoad="load">
+                        <template v-for="(pokemon, index) in pokeList" :key="pokemon">
+                            <div :class="['ma-2']">
+                                <v-card
+                                    :class="['mx-auto', 'card', 'pokemon-cards', 'ma-2', 'rounded-xl', 'bg-grey-lighten-2', 'd-flex', 'align-center', 'ga-9']"
+                                    variant="elevated">
+                                    <v-img v-bind:src='pokemon.sprites' max-width="150px"></v-img>
+                                    <v-card-subtitle>
+                                        ID: {{ pokemon.id }}
+                                    </v-card-subtitle>
+                                    <h1> {{ pokemon.name }}</h1>
+                                    <v-card-text class="btn-info">
+                                        <v-btn class="btn-info-pokemon" color="primary"> More Info </v-btn>
+                                    </v-card-text>
+                                </v-card>
+                            </div>
+                        </template>
+                    </v-infinite-scroll>
                 </v-card-text>
                 <v-card-text class="pokeinfo">
-                    <PokemonInfo />
+                    <v-card
+                        class="mx-auto card pokemon-cards ma-2 rounded-xl bg-grey-lighten-2 d-flex align-center ga-9">
+                        <v-img max-width="150px"></v-img>
+                        <v-card-subtitle>
+                            ID: 
+                        </v-card-subtitle>
+                        <h1> </h1>
+                        <v-card-text class="btn-info">
+                            
+                        </v-card-text>
+                    </v-card>
                 </v-card-text>
             </span>
         </v-card>
@@ -25,13 +52,49 @@
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 
+const pokeList = ref([])
 const pokeCount = ref('')
+const pokeListNext = ref('https://pokeapi.co/api/v2/pokemon?offset=0&limit=5')
 
-onMounted(async () => {
-    const pokeData = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=5')
-        .then(response => response.json())
-    pokeCount.value = pokeData.count
-})
+
+async function api() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(fetch(pokeListNext.value).then(response => response.json()))
+        }, 1000)
+    })
+}
+
+async function pokeInfo(url: string) {
+    const response = await fetch(url).then(response => response.json())
+    const { id, sprites } = response;
+    return response
+}
+
+async function load({ done }) {
+    const res = await api()
+    const { results } = res
+
+    const pokePayload = await Promise.all(results.map(async pokemon => {
+        const { id, sprites } = await pokeInfo(pokemon.url)
+        return {
+            name: pokemon.name,
+            id,
+            sprites: sprites.other.dream_world.front_default,
+        }
+    }))
+    pokeCount.value = res.count
+    pokeListNext.value = res.next
+    pokeList.value.push(...pokePayload)
+    done('ok')
+
+}
+// onMounted(async () => {
+//     const pokeData = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=5')
+//         .then(response => response.json())
+//     pokeList.value = pokeData.results
+//     pokeListNext.value = pokeData.next
+// })
 </script>
 
 <style>
@@ -60,11 +123,9 @@ onMounted(async () => {
 .pokeinfo {
     flex-grow: 0.4;
 }
-
-.span-poke {
+.span-poke{
     height: 47rem;
 }
-
 .pokemon-cards {
     @media (max-width: 599px) {
         padding: 2rem;
